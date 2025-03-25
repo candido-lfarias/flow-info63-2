87e482b8ba1c956638e842b2347802aa90cf1473
candidocimol <candido.cimol@gmail.com>
2025-03-25 14:53:52 -0300

    Atualização Cândido

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/candido-lfarias', async (req, res) => {
+	const fs = require('fs').promises;
+	const data = await fs.readFile('candido-lfarias.txt', 'utf8');
+	res.json(data);
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
