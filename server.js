45b785ea28b82971f692b579bf051b9f1991e079
Amanda <euamandaferreira7@gmail.com>
2025-03-25 14:32:30 -0300

    Atualização de tarefa - amanda-6575890

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/amanda-6575890', (req, res) => {
+  res.json("amanda-6575890.txt");
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
