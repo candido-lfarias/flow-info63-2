a78ceb4ae2f16e98a339bc0ab07419d137244663
Raul <rauloliveira08@gmail.com>
2025-03-25 14:52:09 -0300

    Atualização da tarefa - raul-6704946

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/raul-6704946', (req, res) => {
+  res.json('raul-6704946.txt');
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
