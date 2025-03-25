512f6813a75430ac719c2e073ef60944052dcfb6
Isa Gularte <isagularte09@gmail.com>
2025-03-25 15:21:30 -0300

    Atualização de tarefa - isadora-gularte

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-
-
 app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
-app.get('/api/isadora-gularte', (req, res) => {
-  res.json('isadora-gularte.txt');
+app.get('/api/isadora-gularte', async (req, res) => {
+  const fs = require('fs') .promisses
+  const arquivo = await fs.readFile('isadora-gularte.txt', 'utf8');
+  res.json(arquivo)
 });
 
 
