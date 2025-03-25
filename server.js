a5b33e4fc1a646ff4c3355ee6182387de8700099
Raíssa Reuter <raissareuter@gmail.com>
2025-03-25 15:22:15 -0300

    Adiciona sistema de login

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 
-
-
 app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/raissa-6541013', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile('raissa-6541013.txt','utf8')
+  res.json(JSON.parse(data));
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
