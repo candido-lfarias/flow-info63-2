5fa6676adaca896778e0fd5943b2ed261225f6f0
Isabella <Isabella.emerichs@gmail.com>
2025-03-25 15:32:51 -0300

    Atualização de tarefa

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
+
 app.use(express.json());
-
-
 
 app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/isabella-dedrosa', async (req, res) => {
+  const fs = require ('fs').promises;
+  const data = await fs.readFile ('isabella-dedrosa.txt', 'utf-8')
 
+  res.json(JSON.parse(data));
+})
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
