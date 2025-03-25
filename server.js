620f98e31a474bf8b803bccb6212992a660ccdd0
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-03-25 14:31:57 -0300

    Atualização da tarefa - bruno-6575869

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
+app.get('/api/bruno-6575869', (req, res) => {
+  //ler conteúdo do arquivo
+  res.json('bruno-6575859.txt');
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
