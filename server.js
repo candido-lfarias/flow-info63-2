caa671312196af30e060d388c8373a1875a2adb5
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-03-25 14:58:53 -0300

    Atualização de tarefa - bruno-6575869

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
+
 app.use(express.json());
-
-
 
 app.get('/api/version', (req, res) => {
   res.json({ version: '1.0.0' });
 });
 
-app.get('/api/bruno-6575869', (req, res) => {
-  //ler conteúdo do arquivo
-  res.json('bruno-6575859.txt');
+app.get('/api/bruno-6575869', async (req, res) => {
+  const fs = require('fs').promises;
+  const data = await fs.readFile('bruno-6575869.txt', 'utf8');
+  res.json(data);
 });
 
 app.listen(port, () => {
